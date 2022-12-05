b751b5a6e9e1b6ca6b5bf101e2dbcf061f2ff9d1
Ale-PerazaGlez <perazaglezalejandro@gmail.com>
2022-12-05 14:45:27 +0100

    Minor change

--- Day 1/day_1.py
+++ Day 1/day_1.py
@@ -1,6 +1,5 @@
 filename = 'initial_input'
 
 lines = open(filename).read().split('\n\n')
-# elf_list = [x.split() for x in lines]
 elf_list = [[int(x) for x in elf.split()] for elf in lines]
 print(sum(sorted([sum(elf) for elf in elf_list], reverse = True)[:3]))
